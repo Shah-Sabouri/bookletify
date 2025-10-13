29f22a1b648f44556cffa27cf3b5b212c23ee0ef
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-10-14 00:03:49 +0200

    fix: use process.env.PORT for production deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -19,7 +19,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/reviews", reviewRoutes);
 app.use("/api/favorites", favoriteRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 connectDB().then(() => {
-    app.listen(3000, () => console.log(`Server running on http://localhost:${3000}`));
+    app.listen(3000, () => console.log(`Server running on port ${PORT}`));
 });
